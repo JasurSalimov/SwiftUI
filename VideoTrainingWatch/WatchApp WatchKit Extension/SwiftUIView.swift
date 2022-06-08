4b4e593525777a5949e0c557eba718849c50a9e5
Jasur Salimov <jasurtsalimov@gmail.com>
2022-06-08 14:33:08 +0500

    UIfixes and adjustments across all watch devices

--- VideoTrainingWatch/WatchApp WatchKit Extension/SwiftUIView.swift
+++ VideoTrainingWatch/WatchApp WatchKit Extension/SwiftUIView.swift
@@ -30,48 +30,25 @@ struct SwiftUIView: View {
         return WKInterfaceDevice.current().screenBounds.width
     }
     
-// This was the best solution for me at the time to solve the constraint related stuff
-    var customPg: CGFloat{
-        let width = WKInterfaceDevice.current().screenBounds.width
-        switch(width){
-        case 396.0: // series 7 44 mm
-            return 8
-        case 352.0: // series 7 41 mm // passed
-            return 0
-        case 368.0: // series 5,6 44mm
-            return 0
-        default: // series 5,6 40mm and others
-            return 4
-        }
-    }
-    
+
     @State var isPaused: Bool
     var totalSetTime: Int
     @State var progressInSeconds: CGFloat
-    let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
     
     var body: some View {
         BackgroundProgress(width: progressWidth, height: screenHeight)
             .frame(width: screenWidth)
             .overlay{
                 TimelineView(.periodic(from: .now, by: 1)) { context in
-                    VStack(alignment: .leading, spacing: 0, content: {
-                        Text("OU7")
-                            .font(.system(size: 17, weight: .medium, design: .default))
-                            .padding(.leading, 15 + customPg)
-                        Text(":\(totalSetTime - Int(progressInSeconds))")
-                                .font(.system(size: 44, weight: .bold, design: .default))
-                                .padding(.leading,7 + customPg)
-                        Text("The name of the TrainingThe name of the Training")
-                            .foregroundColor(.white)
-                            .opacity(0.8)
-                            .lineLimit(nil)
-                            .padding(.leading, 7 + customPg)
-                            .font(.system(size: 12, weight: .bold, design: .default))
+                    VStack(alignment: .center, spacing: 0, content: {
+                        TextView(totalSetTime: totalSetTime, progressInSeconds: progressInSeconds)
+                        Spacer()
                         ControlView(isPaused: isPaused)
                         Spacer()
                         BottomView(calories: 254, heartRate: 180)
                     })
+                    .frame(width: screenWidth, height: screenHeight, alignment: .center)
                     .padding(EdgeInsets(top: 12, leading:32, bottom: 12, trailing: 32))
                 }
             }
@@ -83,7 +60,7 @@ struct SwiftUIView: View {
     
     func invalidateProgress(){
         let percent:CGFloat = screenWidth/CGFloat(totalSetTime)
-        progressInSeconds += 0.25
+        progressInSeconds += 0.01
         progressWidth = CGFloat(progressInSeconds)*percent
         if(Int(progressInSeconds) == totalSetTime){
             debugPrint("Timer stops")
@@ -119,19 +96,6 @@ struct ControlView: View {
     var screenWidth: CGFloat{
         return WKInterfaceDevice.current().screenBounds.width
     }
-    var customPg: CGFloat{
-        let width = WKInterfaceDevice.current().screenBounds.width
-        switch(width){
-        case 396.0: // series 7 44 mm
-            return 8
-        case 352.0: // series 7 41 mm // passed
-            return 0
-        case 368.0: // series 5,6 44mm
-            return 0
-        default: // series 5,6 40mm and others
-            return 0
-        }
-    }
     var isPaused: Bool
     var actionIcon: String{
         if isPaused{
@@ -140,45 +104,73 @@ struct ControlView: View {
             return "pauseIcon"
         }
     }
+    var spacerLength: CGFloat{
+        return (screenWidth - 164)/2
+    }
     var body: some View {
-        HStack{
-            Spacer()
-            Image("previousIcon")
-                .resizable()
-                .clipped()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 34)
-                .onTapGesture(perform: {
-                   previousPressed()
+        HStack(alignment: .center){
+            Spacer(minLength: spacerLength)
+            ZStack(alignment: .center, content: {
+                Button(action: {}, label: {
                 })
-            Spacer(minLength: 11 + customPg)
-            Image("pauseIcon")
-                .resizable()
+                .blur(radius: 10)
+                .frame(width: 40, height: 40, alignment: .center)
+                .background(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.2))
+                .clipped()
+                Image("previousIcon")
+                    .resizable()
+                    .clipped()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20, height: 20, alignment: .center)
+            })
+               .frame(width: 40, height: 40, alignment: .center)
+               .clipped()
+               .cornerRadius(20)
+            Spacer(minLength: 10)
+            Button(action: {actionPressed()}, label: {
+                ZStack{
+                    Button(action: {}, label: {
+                    })
+                    .blur(radius: 10)
+                    .frame(width: 58, height: 58, alignment: .center)
+                    .background(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.2))
+                    .clipped()
+                    Image("pauseIcon")
+                        .resizable()
+                        .clipped()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 40, alignment: .center)
+                }
+                
+            })
                 .frame(width: 58)
                 .clipped()
-                .aspectRatio(contentMode: .fit)
-                .onTapGesture(perform: {
-                     actionPressed()
+                .cornerRadius(58)
+            Spacer(minLength: 10)
+            ZStack(alignment: .center, content: {
+                Button(action: {}, label: {
                 })
-            Spacer(minLength: 11 + customPg)
-            Image("nexticon")
-                .resizable()
+                .blur(radius: 10)
+                .frame(width: 40, height: 40, alignment: .center)
+                .background(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.2))
                 .clipped()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 34)
-                .onTapGesture(perform: {
-                    nextPressed()
-                })
-            Spacer()
+                Image("nextIcon")
+                    .resizable()
+                    .clipped()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20, height: 20, alignment: .center)
+            })
+               .frame(width: 40, height: 40, alignment: .center)
+               .clipped()
+               .cornerRadius(20)
+               Spacer(minLength: spacerLength)
         }
-        .frame(width: screenWidth, height: 58)
-        .padding(.top, 12)
-        .padding(.leading, 5)
-        .padding(.trailing, 5)
+        .frame(width: screenWidth, height: 58, alignment: .center)
+        .scaledToFill()
     }
     
     private func previousPressed(){
-        
+        debugPrint("Previous Pressed")
     }
     private func actionPressed(){
         
@@ -191,23 +183,10 @@ struct ControlView: View {
 struct BottomView: View {
     var calories: Int
     var heartRate: Int
-    var customPg: CGFloat{
-        let width = WKInterfaceDevice.current().screenBounds.width
-        switch(width){
-        case 396.0: // series 7 44 mm
-            return 8
-        case 352.0: // series 7 41 mm
-            return 0
-        case 368.0: // series 5,6 44mm
-            return 0
-        default: // series 5,6 40mm and others
-            return 0
-        }
-    }
     var screenWidth: CGFloat{
         return WKInterfaceDevice.current().screenBounds.size.width
     }
-    
+
     var body: some View {
         HStack{
             HStack{
@@ -219,8 +198,9 @@ struct BottomView: View {
                     .aspectRatio(contentMode: .fit)
                     .clipped()
             }
-            .frame(width: screenWidth/2 - 27)
-            Spacer(minLength: 27)
+            .frame(alignment: .leading)
+            .padding(.leading, 10)
+            Spacer()
             HStack{
                 Text(String(calories))
                     .font(.system(size: 20, weight: .medium, design: .default))
@@ -230,10 +210,34 @@ struct BottomView: View {
                     .aspectRatio(contentMode: .fit)
                     .clipped()
             }
-            .frame(width: screenWidth/2 - (19 + customPg), height: nil, alignment: .trailing)
+            .frame(alignment:.trailing)
+            .padding(.trailing, 10)
         }
+        .frame(width: screenWidth, height: nil, alignment: .center)
         .padding(.leading, 12)
         .padding(.trailing, 12)
+        .padding(.bottom, 10)
     }
 }
 
+
+struct TextView: View {
+    var totalSetTime: Int
+    var progressInSeconds: CGFloat
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0, content: {
+            Text("OU7")
+                .font(.system(size: 17, weight: .medium, design: .default))
+                .padding(.leading, 16)
+                .padding(.top, 8)
+            Text(":\(totalSetTime - Int(progressInSeconds))")
+                    .font(.system(size: 44, weight: .bold, design: .default))
+            Text("The name of the TrainingThe name of the Training")
+                .foregroundColor(.white)
+                .opacity(0.8)
+                .lineLimit(nil)
+                .font(.system(size: 12, weight: .bold, design: .default))
+        })
+        .frame(width: WKInterfaceDevice.current().screenBounds.width, height: nil, alignment: .leading)
+    }
+}
