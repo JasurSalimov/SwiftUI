82c7803dc3f2462a86edd7e3d03722e9292c8bcc
Jasur Salimov <jasurtsalimov@gmail.com>
2022-07-12 15:49:36 +0500

    Restart ( Complex Swiftui drag gesture)

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -11,14 +11,39 @@ struct HomeView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     var body: some View {
         VStack {
-            Text("Home")
-                .font(.largeTitle)
-            
+            //MARK: - Header
+            Spacer()
+            ZStack{
+                
+            }
+            ZStack {
+                CircleGroupView(shapeColor: Color.gray, shapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            //MARK: - Center
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            //MARK: - Footer
+            Spacer()
             Button(action: {
                 isOnboardingViewActive = true
-            }, label: {
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text("Restart")
-            })
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
     }
 }
