92353e098013909090d2a695980d889d44e9268a
Jasur Salimov <jasurtsalimov@gmail.com>
2022-07-13 12:09:50 +0500

    Initial setup 'Pinch App'

---
+++ Pinch App/Pinch App/Pinch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_AppApp.swift
+//  Pinch App
+//
+//  Created by Jasur Salimov on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
