dfb642e47cde66702acd6f581ba48cd06aea2720
Jasur Salimov <jasurtsalimov@gmail.com>
2022-07-15 10:57:30 +0500

    Fruits app start

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Jasur Salimov on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
