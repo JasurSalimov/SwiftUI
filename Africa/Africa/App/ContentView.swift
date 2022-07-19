5a5124a6fef32288820a9022dd84ccbae6d43f71
Jasur Salimov <jasurtsalimov@gmail.com>
2022-07-19 17:18:26 +0500

    Starts Grid Layout in main tab

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    private let haptics = UIImpactFeedbackGenerator(style: .medium)
+    @State private var isGridViewActive: Bool = false
     
     var body: some View {
         NavigationView{
@@ -26,7 +28,30 @@ struct ContentView: View {
             .listStyle(.plain)
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
-        }
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    HStack(spacing: 16){
+                        // List
+                        Button(action: {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }){
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary :.accentColor)
+                        }//button
+                        Button(action: {
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }){
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }//button
+                    }//:HStack
+                }//Item
+            }//Bar
+        }// Navigation
     }
 }
 
--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -8,9 +8,67 @@
 import SwiftUI
 
 struct GalleryView: View {
+    //MARK: - Properties
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    @State private var selectedAnimal: String = "lion"
+    //Simple Grid
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+     
+    //Efficient Grid Definition
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    //Dynamic Grid Layout
+
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch(){
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumn))
+    }
+    //MARK: - Body
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
-            
+            VStack(alignment: .center, spacing: 30){
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                //MARK: - Slider
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: {value in
+                        withAnimation(.easeIn){
+                            gridSwitch()
+                        }
+                    })
+                
+                //MARK: - Grid
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+                    ForEach(animals){ item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                hapticImpact.impactOccurred()
+                            }
+                    }
+                }//: Grid
+                .onAppear(){
+                    gridSwitch()
+                }
+            }//: Vstack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
